d4875423a3c1c44192854a53e16e66f15ec6c157
anandinkasgh9 <nandasgh19@gmail.com>
2025-04-10 18:01:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function addTodo() {
+    const input = document.getElementById('todo-input');
+    const text = input.value.trim();
+  
+    if (text !== '') {
+      const li = document.createElement('li');
+      li.textContent = text;
+      document.getElementById('todo-list').appendChild(li);
+      input.value = '';
+    }
+  }
+  
